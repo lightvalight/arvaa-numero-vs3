b0371b67332d19c804db4d47c139f086e5553423
lightvalight <199157259+lightvalight@users.noreply.github.com>
2025-02-16 23:07:12 +0200

    Update vire.config.js

--- vire.config.js
+++ vire.config.js
@@ -1,13 +1,11 @@
+///<reference types="vite/client"/>
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
-    outDir: 'dist',
-  },
+    outDir: 'dist', rollupOptions:{input:{main:'/index.html'}}},
+  base:'./',
   server: {
     port: 3000
-  }
-})
+  }})
